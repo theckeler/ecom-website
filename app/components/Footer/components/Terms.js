0cc00ae8ef634e1a4c01f13e37365578bfa3ddec
Todd Heckeler <todd@consumedesign.com>
2023-07-12 14:25:09 -0400

    footer again

--- app/components/Footer/components/Terms.js
+++ app/components/Footer/components/Terms.js
@@ -2,26 +2,26 @@
 
 export default function FooterTerms() {
 	return (
-		<ul className="flex items-center flex-col md:flex-row">
+		<ul className="flex md:items-center flex-col md:flex-row">
 			<li className="font-bold">© 2022&nbsp;Cubcadet. All Rights Reserved.</li>
 			<li className="md:ml-auto">
 				<a
 					href="https://www.stanleyblackanddecker.com/privacy-policy"
-					className="underline py-4 lg:p-2 block">
+					className="underline py-4 md:p-2 block">
 					Privacy Policy
 				</a>
 			</li>
 			<li className="">
 				<a
 					href="https://www.cubcadet.com/en_US/terms-of-use.html"
-					className="underline py-4 lg:p-2 block">
+					className="underline py-4 md:p-2 block">
 					Terms of Use
 				</a>
 			</li>
 			<li className="">
 				<a
 					href="https://www.cubcadet.com/en_US/online-store-policies.html"
-					className="underline py-4 lg:p-2 block">
+					className="underline py-4 md:p-2 block">
 					Online Store Policies
 				</a>
 			</li>

deef79c79f60047df74023b31c380dd0826ce8a3
Todd Heckeler <todd@consumedesign.com>
2023-07-14 16:44:19 -0400

    fixing top

--- app/lawn-mowers/riding-lawn-mowers/zero-turn-mowers/page.js
+++ app/lawn-mowers/riding-lawn-mowers/zero-turn-mowers/page.js
@@ -50,7 +50,7 @@ export default function Index() {
 				</p>
 			</li>
 
-			<li className="sticky top-0 z-30 bg-gray-200" id="products">
+			<li className="sticky top-12 z-30 bg-gray-200" id="products">
 				<ul className="flex items-center max-w-screen-2xl mx-auto px-3 py-2">
 					<li>
 						<Sort />

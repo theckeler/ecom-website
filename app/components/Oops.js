a413feaf99fcae1d2ea608a36f1172637e19a803
Todd Heckeler <todd@consumedesign.com>
2023-07-17 08:53:01 -0400

    update oops

--- app/components/Oops.js
+++ app/components/Oops.js
@@ -1,18 +1,42 @@
 "use client";
 import Link from "next/link";
-import toggleMenu from "@/functions/toggleMenu";
 
 export default function Oops({ className }) {
+	const oopsList = [
+		{
+			tile: "Category Page (/lawn-mowers/)",
+			url: "/lawn-mowers/",
+		},
+		{
+			tile: "Category Page (/lawn-mowers/riding-lawn-mowers/)",
+			url: "/lawn-mowers/riding-lawn-mowers/",
+		},
+		{
+			tile: "PLP Page",
+			url: "/lawn-mowers/riding-lawn-mowers/zero-turn-mowers#top",
+		},
+		,
+		{
+			tile: "PDP Page",
+			url: "/lawn-mowers/riding-lawn-mowers/zero-turn-mowers/1",
+		},
+	];
+
 	return (
 		<div className="font-bold p-16">
 			Oops... it seems there is no link yet, try this one:
 			<ul className="mt-4">
 				<li>
-					<Link
-						className="bg-amber-200 underline p-2 uppercase block"
-						href="/lawn-mowers/riding-lawn-mowers/zero-turn-mowers#top">
-						/lawn-mowers/riding-lawn-mowers/zero-turn-mowers
-					</Link>
+					{oopsList.map(function (block, i) {
+						return (
+							<Link
+								key={i}
+								className="bg-amber-200 underline p-2 uppercase block mb-2"
+								href={block.url}>
+								{block.tile}
+							</Link>
+						);
+					})}
 				</li>
 			</ul>
 		</div>

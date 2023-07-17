61b3cd7080d8c6841ff65aee5b7469878832d780
Todd Heckeler <todd@consumedesign.com>
2023-07-17 17:27:27 -0400

    comment out for now

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,7 +1,7 @@
 export default function Screen({ className }) {
 	return (
 		<div
-			className="hidden fixed z-30 top-0 left-0 w-full h-full bg-white/70 backdrop-blur-sm"
+			className="hidden fixed z-0 md:z-30 top-0 left-0 w-full h-full bg-white/70 backdrop-blur-sm"
 			id="screen"
 		/>
 	);

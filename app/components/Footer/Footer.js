d292e45fcac17f5414e74c1b01c7e181f25c571c
Todd Heckeler <todd@consumedesign.com>
2023-07-20 13:53:45 -0400

    footer fix

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -54,12 +54,12 @@ export default function Footer() {
 								{ title: "Terms of Use" },
 								{ title: "Online Store Policies" },
 							],
-							className: null,
+							className: "",
 							row: true,
 							alignRight: false,
 							extra: {
 								copy: '<strong>WARRANTY ADDENDUM. IMPORTANT: This addendum defines the start of the warranty period.</strong> The applicable Warranty Period will begin on the original date of purchase of the Product or on the date of delivery of the Product, whichever is later. <strong>Engine Disclaimer: </strong>The engine horsepower information is provided by the engine manufacturer to be used for comparison purposesonly. See your local Cub Cadet Dealer for warranty details.<strong>Pricing Disclaimer:</strong> Posted price is in USD Dollars andis&nbsp;manufacturer&apos;s suggested sale price. Models and pricing mayvary by location. Taxes, freight, set-up&nbsp;and delivery not included.Optional equipment, accessories and attachments sold separately. See yourretailer for details.&nbsp;<strong>Image Disclaimer:</strong>&nbsp;Products may vary from depictedmodel image in design, required attachments, safety features andnon-functional appearance, and may not reflect dealer inventory or unitspecifications.<strong>Specifications Disclaimer:</strong> Specifications subject tochange without notice. Images may not reflect retailer inventory and/orunit specifications.<strong>Operator&apos;s Manual Disclaimer:</strong> The operator&apos;smanual posted is for general information and use. To ensure the downloadof the operator&apos;s manual specific to your unit, we require a modeland serial number.&nbsp;<strong>Speed Disclaimer: </strong>Actual vehicle speed varies based onload, use and environmental conditions.<strong>Battery Disclaimer: </strong>Battery and battery powered productperformance varies with load, use and environmental conditions.<strong>Software Disclaimer:</strong> Software available on Companywebsites is provided on an &apos;&apos;as is&apos;&apos; basis without anywarranty of any kind, either express or implied. The download and use ofany software is done at the user&apos;s own risk.&nbsp;<strong>Professional Products:</strong> Cub Cadet commercial products areintended for professional use.<strong>UTV:</strong> Cub Cadet Utility Vehicles (UTV) are intended foroff-road use by adults only. Please see the operator’s manual and thewarning labels posted on the vehicle itself for more details. <strong>Email disclaimer: </strong>Sign up to receive communication onservices, products and special offers. You may unsubscribe at any time.Please refer to our <a href="?oops=1#Click-the-Zero-Turn-Mowers-link-that-is-the-plp">Privacy Policy</a>.',
-								className: "p-5 text-xs leading-tight",
+								className: "p-5 lg:p-0 lg:pt-8 text-xs leading-tight",
 							},
 						}}
 					/>
